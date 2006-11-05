55b7d078cc2edb6a67ba16ec10af7437bdbcbda1
joerg <joerg@f1c0e022-a177-11df-8cf9-6fc2f28f26a3>
2006-11-05 00:18:09 +0000

    added multiselect lists for band maintenance

---
+++ web/js/utils.js
@@ -0,0 +1,17 @@
+function moveList(fromid,toid) {
+    alert('test');
+    /*
+    aSelFrom = document.getElementById(fromid);
+    aSelTo = document.getElementById(toid);         
+	indexSelTo = aSelTo.options.length-1;
+	for (dd = 0; dd < aSelFrom.options.length; dd++) {
+      if (aSelFrom.options[dd].selected) {
+	    indexSelTo=indexSelTo+1;
+	    aSelTo.options[indexSelTo]=new Option(aSelFrom.options[dd].text,aSelFrom.options[dd].value);
+	    //aSelFrom.options[dd].selected=false;
+	    aSelFrom.options[dd] = null;
+	    dd = dd - 1;
+	  }		
+    }
+    */        
+}
