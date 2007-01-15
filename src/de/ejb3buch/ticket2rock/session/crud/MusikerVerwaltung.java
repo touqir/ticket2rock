bb409c831fde47c2656e761904919eda4d5b260d
joerg <joerg@f1c0e022-a177-11df-8cf9-6fc2f28f26a3>
2007-01-15 22:19:54 +0000

    Bean and its local interface for administration of Musiker

---
+++ src/de/ejb3buch/ticket2rock/session/crud/MusikerVerwaltung.java
@@ -0,0 +1,67 @@
+package de.ejb3buch.ticket2rock.session.crud;
+
+import java.util.Collection;
+
+import javax.ejb.Local;
+
+import de.ejb3buch.ticket2rock.entity.Band;
+import de.ejb3buch.ticket2rock.entity.Musiker;
+
+@Local
+public interface MusikerVerwaltung {
+
+    /**
+     * Legt eine Musiker-Entität in der Persistenzschicht an
+     * @param musiker Musiker-POJO mit den Attributen der Band
+     */
+	public void createMusiker(Musiker musiker);
+	
+	/**
+	 * Selektiert eine Musiker-Entität mit einen gegebenen Namen
+	 * @param name Name der Musiker, die selektiert werden soll
+	 * @return Musiker Objekt, das den bestimmten Namen besitzt.
+	 * Existiert kein Musiker mit diesem Namen, ist der Rückgabewert null
+	 */
+	public Musiker getMusikerByName(String name);
+
+	/**
+	 * Aktualisiert eine Musiker-Entität in der Persistenzschicht
+	 * @param Musiker Musiker Objekt, das persistiert werden soll
+	 */
+	public void updateMusiker(Musiker musiker);
+	/**
+	 * Löscht eine Musiker-Entität aus der Persistenzschicht
+	 * @param musiker id des zu löschenden Musikers
+	 */
+	public void deleteMusiker(Integer musikerId);
+
+//TODO move the band-methods to the interface for bandverwaltung
+	/**
+	 * selektiert alle Musiker-Entitäten
+	 * @return Musiker-Entitäten
+	 */
+	public Collection<Musiker> getMusiker();
+	
+	/**
+	 * selektiert alle Band-Entitäten
+	 * @return Band-Entitäten
+	 */
+	public Collection<Band> getBands();
+	
+	/**
+	 * Selektiert eine Band für eine gegebene Id
+	 * @param bandId
+	 * @return Band Entität, null fallse keine Band-Entität
+	 * mit dieser id existiert
+	 */
+	public Band getBandById(Integer bandId);
+    
+	/**
+	 * Selektiert einen Musiker für eine gegebene Id
+	 * @param musikerId
+	 * @return Musiker Entität, null falls kein Musiker-Entität
+	 * mit dieser id existiert
+	 */
+	public Musiker getMusikerById(Integer musikerId);
+
+}
---
+++ src/de/ejb3buch/ticket2rock/session/crud/MusikerVerwaltungBean.java
@@ -0,0 +1,121 @@
+package de.ejb3buch.ticket2rock.session.crud;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+import javax.ejb.EJBException;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.apache.log4j.Logger;
+
+import de.ejb3buch.ticket2rock.entity.Band;
+import de.ejb3buch.ticket2rock.entity.Musiker;
+
+/**
+ * Stateless Session Bean zur Verwaltung der Musiker Entitäten.
+ *
+ */
+@Stateless
+public class MusikerVerwaltungBean implements MusikerVerwaltung {
+
+	static Logger logger = Logger.getLogger(MusikerVerwaltungBean.class);
+
+	@PersistenceContext
+	private EntityManager em;
+
+
+
+    /**
+     *@inheritDoc
+     */
+	@SuppressWarnings("unchecked")
+	public Musiker getMusikerByName(String name) {
+		Query query = em.createQuery("from Musiker i where i.name = :name");
+		query.setParameter("name", name);
+		// query.setParameter("typ",interpretType);
+		List<Musiker> musiker = (List<Musiker>) query.getResultList();
+		if ((musiker == null) || (musiker.isEmpty())) {
+			return null;
+		}
+		return musiker.iterator().next();
+	}
+
+    /**
+     *@inheritDoc
+     */
+	public void createMusiker(Musiker musiker) {
+		em.persist(musiker);
+	}
+
+    /**
+     *@inheritDoc
+     */
+	public void updateMusiker(Musiker musiker) {
+		em.merge(musiker);
+	}
+
+    /**
+     *@inheritDoc
+     */	
+	public void deleteMusiker(Integer musikerId) {
+		Musiker musiker = em.find(Musiker.class,musikerId.intValue());
+		try {
+
+			Set<Band> musikerBands = (Set<Band>) musiker.getBands();
+			if (musikerBands != null) {
+				for (Band band : musikerBands) {
+					Set<Musiker> bandMusiker = band.getMusiker();
+					if ((bandMusiker != null) && (!bandMusiker.isEmpty())) {
+						logger.debug("removed band " + band.getName()
+								+ " from Musiker " + musiker.getName());
+						bandMusiker.remove(musiker);
+					}
+				}
+			}
+			em.remove(musiker);
+
+		} catch (Exception ex) {
+			logger.error(ex.getMessage());
+			ex.printStackTrace();
+			throw new EJBException("Fehler beim Löschen eines Musikers");
+		}
+
+	}
+	
+	
+    /**
+     *@inheritDoc
+     */
+	@SuppressWarnings("unchecked")
+	public Collection<Musiker> getMusiker() {
+		Query query = em.createQuery("from Musiker");
+		return (Collection<Musiker>) query.getResultList();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Band> getBands() {
+		List resultList = em.createQuery(
+				"FROM Interpret i WHERE typ='B' ORDER BY name").getResultList();
+		logger.debug("Anzahl der gefundenen Bands: " + resultList.size());
+		return resultList;
+	}
+
+    /**
+     *@inheritDoc
+     */	
+	public Band getBandById(Integer bandId) {
+		return em.find(Band.class,bandId);
+	}
+
+    /**
+     *@inheritDoc
+     */	
+	public Musiker getMusikerById(Integer musikerId) {
+		return em.find(Musiker.class,musikerId);
+	}
+
+}
