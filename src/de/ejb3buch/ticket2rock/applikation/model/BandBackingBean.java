489e53f5a588af3a8b2d759ae81bec67ab608cc4
joerg <joerg@f1c0e022-a177-11df-8cf9-6fc2f28f26a3>
2007-01-15 22:07:11 +0000

    wrapps Band Persistent Entity

--- src/de/ejb3buch/ticket2rock/applikation/model/BandBackingBean.java
+++ src/de/ejb3buch/ticket2rock/applikation/model/BandBackingBean.java
@@ -2,66 +2,80 @@ package de.ejb3buch.ticket2rock.applikation.model;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import de.ejb3buch.ticket2rock.entity.Band;
+import de.ejb3buch.ticket2rock.entity.Musiker;
 
 /**
  * Band Baking Bean für die Verwendung im JSF-Frameworks
+ * 
  * @author Jochen Jörg
  */
 public class BandBackingBean {
-   
-	private Integer id = null;
-	private String name = null;;
-	private Collection<String> musikerNamensListe = null;;
-	private Collection<String> musikerIdListe = null;
-	
-	public BandBackingBean () {
 
+	private Band band = null;
+
+	private Collection<String> musikerIdListe = null;
+
+	public BandBackingBean() {
+		band = new Band();
 	}
-	
-	public BandBackingBean (Integer aId, String aName) {
-		id = aId;
-		name = aName;
+
+	public BandBackingBean(Band aBand) {
+		this.band = aBand;
 	}
 
 	public String getName() {
-		return name;
+		return band.getName();
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setName(String aName) {
+		this.band.setName(aName);
 	}
 
 	public Integer getId() {
-		return id;
-	}
-
-
-	public void setMusikerNamensListe(Collection<String> musikerNamen) {
-		this.musikerNamensListe = musikerNamen;		
-	}
-
-	public Collection<String> getMusikerNamensListe() {
-		return musikerNamensListe;
+		return new Integer(band.getId());
 	}
 	
-	public List<String> getMusikerListeKommaGetrennt() {
-		
-		if  (musikerNamensListe == null) {
-			return null;
-		}
-		List<String> list = new ArrayList<String>();
-		Iterator iter = musikerNamensListe.iterator();
-		boolean isFirst = true;
-		while (iter.hasNext()) {
-			if (isFirst) {
-				list.add((String)iter.next());
-				isFirst = false;
+	/**
+	 * 
+	 * @return Collection der Namen der Musiker der Band, die durch den
+	 *         BandBackingBean representiert wird
+	 */
+	public Collection<String> getMusikerNamensListe() {
+
+		List<String> musikerNamensListe = new ArrayList<String>();
+		Set<Musiker> musikerList = band.getMusiker();
+		if ((musikerList != null) && (!musikerList.isEmpty())) {
+			for (Musiker musiker : musikerList) {
+				musikerNamensListe.add(musiker.getName());
 			}
-			else {
-				list.add(", " + (String) iter.next());
-			}			
+		}
+
+		return musikerNamensListe;
+	}
+
+	/**
+	 * Erzeugt eine kommagetrennte List der Musikernamen
+	 * 
+	 * @return kommagetrennte Liste der Bandmusiker-Namen
+	 */
+	public List<String> getMusikerListeKommaGetrennt() {
+        if (this.getMusikerNamensListe() == null) {
+        	return null;
+        }
+		List<String> list = new ArrayList<String>();
+		boolean isFirst = true;
+		for (String name : getMusikerNamensListe()) {
+			if (isFirst) {
+				list.add(name);
+				isFirst = false;
+			} else {
+				list.add(", " + name);
+			}
 		}
 		return list;
 	}
@@ -70,9 +84,28 @@ public class BandBackingBean {
 		return musikerIdListe;
 	}
 
+	/**
+	 * Instanziiere ein Musiker Objekt prop übergebene Id. Lege die Musiker
+	 * Objekte in ein Set ab und weise dieses dem Band Objekt zu 
+	 * @param musikerIdListe String Collection bestehend aus musiker Ids
+	 */
 	public void setMusikerIdListe(Collection<String> musikerIdListe) {
-		this.musikerIdListe = musikerIdListe;
+		Set<Musiker> musikerSet = new HashSet<Musiker>();
+		for (String musikerId:musikerIdListe) {
+			Musiker musiker = new Musiker();
+			musiker.setId(Integer.parseInt(musikerId));
+			musikerSet.add(musiker);
+		}		
+		band.setMusiker(musikerSet);
 	}
 
-	
+	/**
+	 * gibt das persistent Band Entity Objekt zurück, das durch die BackingBean
+	 * gewrapped ist.
+	 * @return Band Persistent Entity Objekt
+	 */
+	public Band getBand() {
+		return this.band;
+	}
+
 }
