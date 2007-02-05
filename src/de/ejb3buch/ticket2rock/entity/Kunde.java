46720014a404517e281a8cc35f014728df14ee4f
koschek <koschek@f1c0e022-a177-11df-8cf9-6fc2f28f26a3>
2007-02-05 21:49:28 +0000

    Erstmaliges Ersuchen um Versionskontrolle.

---
+++ src/de/ejb3buch/ticket2rock/entity/Kunde.java
@@ -0,0 +1,75 @@
+/**
+ *  Ticket2Rock ist die Beispielanwendung des Buchs "EJB 3 professionell" (dpunkt).
+ *  Es implementiert eine einfache Webanwendung zur Onlinebuchung von Tickets für
+ *  Rockkonzerte auf Basis von EJB 3.0 und JavaServer Faces.
+ *
+ *  Copyright (C) 2006
+ *  Dierk Harbeck, Stefan M. Heldt, Oliver Ihns, Jochen Jörg und Holger Koschek
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package de.ejb3buch.ticket2rock.entity;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ * Ein Kunde kann Konzerttickets bestellen. Er wird derzeit nur über eine
+ * E-Mail-Adresse identifiziert.
+ */
+
+@Entity
+public class Kunde {
+
+	private int id;
+
+	private String email;
+
+	private List<Ticketreservierung> bestellungen;
+
+	@Id
+	@GeneratedValue
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "kunde")
+	public List<Ticketreservierung> getBestellungen() {
+		return this.bestellungen;
+	}
+
+	public void setBestellungen(List<Ticketreservierung> bestellungen) {
+		this.bestellungen = bestellungen;
+	}
+}
