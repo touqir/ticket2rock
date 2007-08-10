2e59e3f75e5bc42e0c309703a653569a620770a5
koschek <koschek@f1c0e022-a177-11df-8cf9-6fc2f28f26a3>
2007-08-10 08:57:30 +0000

    Korrigiert (falsche Annahmen, die BeanStatisticsBean betreffend; diese basiert auf einem Singleton, und es kann nicht davon ausgegangen werden, dass dieses "leer" ist)

--- test/src/de/ejb3buch/ticket2rock/session/statistics/BeanStatisticsBeanTest.java
+++ test/src/de/ejb3buch/ticket2rock/session/statistics/BeanStatisticsBeanTest.java
@@ -25,64 +25,103 @@
 package de.ejb3buch.ticket2rock.session.statistics;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertNotNull;
 
 import java.lang.reflect.Method;
 
 import org.junit.Test;
 
-public class BeanStatisticsBeanTest
-{
-    /**
-     * Testen der reportMethodCall Methode. Wir sehen einfach mal nach, ob
-     * <ul>
-     * <li> ein Method Call auch nur genau einmal geloggt wird</li>
-     * <li> ein anderer Method Call anders geloggt wird</li>
-     * <li> ein weiterer MethodCall zurErhoehung des Counters fuehrt</li>
-     * </ul>
-     */
-    @Test
-    public void testReportMethodCall()
-    {
-        BeanStatisticsBean bs = new BeanStatisticsBean();
+public class BeanStatisticsBeanTest {
+	/**
+	 * Testen der reportMethodCall Methode. Wir sehen einfach mal nach, ob
+	 * <ul>
+	 * <li> ein Method Call auch nur genau einmal geloggt wird</li>
+	 * <li> ein anderer Method Call anders geloggt wird</li>
+	 * <li> ein weiterer MethodCall zurErhoehung des Counters fuehrt</li>
+	 * </ul>
+	 */
+	@Test
+	public void testReportMethodCall() {
+		BeanStatisticsBean bs = new BeanStatisticsBean();
 
-        Method dummyMethodOne = this.getClass().getMethods()[0];
-        Method dummyMethodTwo = this.getClass().getMethods()[1];
+		Method dummyMethodOne = this.getClass().getMethods()[0];
+		Method dummyMethodTwo = this.getClass().getMethods()[1];
 
-        // noch nichts geloggt - muss 0 liefern
-        assertEquals(0, bs.getMethodUsage().size());
-        bs.reportMethodCall(dummyMethodOne);
+		// mal schau'n, wie viele Methoden schon aufgerufen wurden
+		int currentMethodUsage = bs.getMethodUsage().size();
 
-        // nach dem ersten Aufruf muss ein Element vorhanden sein
-        // und als Counter 1 liefern
-        assertEquals(1, bs.getMethodUsage().size());
-        assertEquals(1, bs.getMethodUsage().get(dummyMethodOne));
+		boolean methodOneHasBeenCalledBefore = (bs.getMethodUsage().get(
+				dummyMethodOne) != null);
+		boolean methodTwoHasBeenCalledBefore = (bs.getMethodUsage().get(
+				dummyMethodTwo) != null);
 
-        bs.reportMethodCall(dummyMethodOne);
-        assertEquals(2, bs.getMethodUsage().get(dummyMethodOne));
+		int methodOneUsage = 0;
+		if (methodOneHasBeenCalledBefore) {
+			methodOneUsage = bs.getMethodUsage().get(dummyMethodOne);
+		}
 
-        // eine andere Methode muss immer noch 0 bzw null liefern
-        assertEquals(null, bs.getMethodUsage().get(dummyMethodTwo));
+		int methodTwoUsage = 0;
+		if (methodTwoHasBeenCalledBefore) {
+			methodTwoUsage = bs.getMethodUsage().get(dummyMethodTwo);
+		}
 
-    }
+		// nach dem ersten Aufruf muss ein Element vorhanden sein
+		// und als Counter methodOneUsage+1 liefern
+		bs.reportMethodCall(dummyMethodOne);
+		assertNotNull((bs.getMethodUsage().get(dummyMethodOne)));
 
-    @Test
-    public void testReportMethodDuration()
-    {
-        BeanStatisticsBean bs = new BeanStatisticsBean();
+		if (!methodOneHasBeenCalledBefore) {
+			assertEquals(currentMethodUsage + 1, bs.getMethodUsage().size());
+		}
+		assertEquals(methodOneUsage + 1, bs.getMethodUsage()
+				.get(dummyMethodOne));
 
-        Method dummyMethodOne = this.getClass().getMethods()[0];
+		bs.reportMethodCall(dummyMethodOne);
+		assertEquals(methodOneUsage + 2, bs.getMethodUsage()
+				.get(dummyMethodOne));
 
-        // noch nichts geloggt - muss 0 liefern
-        assertEquals(0, bs.getMethodTotalDuration().size());
+		// eine andere Methode muss immer noch 0 bzw null liefern
+		if (!methodTwoHasBeenCalledBefore) {
+			assertNull(bs.getMethodUsage().get(dummyMethodTwo));
+		} else {
+			assertEquals(methodTwoUsage, bs.getMethodUsage()
+					.get(dummyMethodTwo));
+		}
+	}
 
-        // einmal loggen mit Dauer = 100
-        bs.reportMethodDuration(dummyMethodOne, 100L);
-        assertEquals(100L, bs.getMethodTotalDuration().get(dummyMethodOne));
+	@Test
+	public void testReportMethodDuration() {
+		BeanStatisticsBean bs = new BeanStatisticsBean();
 
-        // nochmal loggen mit Dauer = 400
-        bs.reportMethodDuration(dummyMethodOne, 400L);
-        assertEquals(500L, bs.getMethodTotalDuration().get(dummyMethodOne));
+		Method dummyMethodOne = this.getClass().getMethods()[0];
 
-    }
+		// mal schau'n, wie lange die Methodenaufrufe bisher gedauert haben
+		int currentMethodDuration = bs.getMethodTotalDuration().size();
+
+		boolean methodOneHasBeenCalledBefore = (bs.getMethodUsage().get(
+				dummyMethodOne) != null);
+
+		Long methodOneDuration = 0L;
+		if (methodOneHasBeenCalledBefore) {
+			methodOneDuration = bs.getMethodTotalDuration().get(dummyMethodOne);
+		}
+
+		// einmal loggen mit Dauer = 100
+		bs.reportMethodDuration(dummyMethodOne, 100L);
+		if (!methodOneHasBeenCalledBefore) {
+			assertEquals(currentMethodDuration + 1, bs.getMethodTotalDuration()
+					.size());
+		}
+		assertNotNull(bs.getMethodTotalDuration().get(dummyMethodOne));
+		assertEquals(methodOneDuration + 100L, bs.getMethodTotalDuration().get(
+				dummyMethodOne));
+
+		// nochmal loggen mit Dauer = 400
+		bs.reportMethodDuration(dummyMethodOne, 400L);
+		assertEquals(methodOneDuration + 500L, bs.getMethodTotalDuration().get(
+				dummyMethodOne));
+
+	}
 
 }
