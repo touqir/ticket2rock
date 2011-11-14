e8c0b188d3667947ef76e5f448659493179edddd
CarlDuevel <CarlDuevel@f1c0e022-a177-11df-8cf9-6fc2f28f26a3>
2011-11-14 15:41:25 +0000

    update von ruby client.

--- ticket2rock/client/AuskunftWSClient.rb
+++ ticket2rock/client/AuskunftWSClient.rb
@@ -1,9 +1,9 @@
 require 'soap/rpc/driver'
 
-server = 'http://localhost:8080/ticket2rock/AuskunftBean'
-namespace = 'http://session.ticket2rock.ejb3buch.de/jaws'
-ws = SOAP::RPC::Driver.new(server, namespace)
+endpointUrl = 'http://localhost:8080/ticket2rock/KonzertInfo/AuskunftBean' 
+namespace = 'http://session.ticket2rock.ejb3buch.de'
+ws = SOAP::RPC::Driver.new(endpointUrl ,namespace)
 #ws.wiredump_dev = STDOUT
-ws.add_method('sucheKonzerteWeb', 'String_1', 'Date_1', 'Date_2')
+ws.add_method('sucheKonzerteWeb', 'Ortsname', 'Startdatum', 'Enddatum')
 result = ws.sucheKonzerteWeb(nil , nil , nil)
 print result
