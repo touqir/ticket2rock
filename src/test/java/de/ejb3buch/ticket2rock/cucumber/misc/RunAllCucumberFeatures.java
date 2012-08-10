b0af721419a2b7dd787b83f1ce499e561ef774c4
Carl Anders Düvel <c.a.duevel@gmail.com>
2012-08-10 17:53:52 +0200

    Upgrade for cucumber & selenium + Adding screenshot to html report

--- src/test/java/de/ejb3buch/ticket2rock/cucumber/misc/RunAllCucumberFeatures.java
+++ src/test/java/de/ejb3buch/ticket2rock/cucumber/misc/RunAllCucumberFeatures.java
@@ -3,10 +3,10 @@ package de.ejb3buch.ticket2rock.cucumber.misc;
 import org.junit.runner.RunWith;
 
 import cucumber.junit.Cucumber;
-import cucumber.junit.Feature;
 
 @RunWith(Cucumber.class)
-@Feature(value = "cucumber/features", packages = "de.ejb3buch.ticket2rock.cucumber.step")
+@Cucumber.Options(features = "src/test/resources/", glue = "de.ejb3buch.ticket2rock", 
+  strict = true, format = "html:target/cucumber")
 public class RunAllCucumberFeatures {
 
 }
--- src/test/java/de/ejb3buch/ticket2rock/cucumber/misc/SharedDriver.java
+++ src/test/java/de/ejb3buch/ticket2rock/cucumber/misc/SharedDriver.java
@@ -1,10 +1,14 @@
 package de.ejb3buch.ticket2rock.cucumber.misc;
 
+import java.io.ByteArrayInputStream;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 
 import cucumber.annotation.After;
+import cucumber.runtime.ScenarioResult;
 
 /**
  * Dieser Driver wird von Cucumber für ein Szenario benutzt und danach
@@ -17,7 +21,18 @@ public class SharedDriver extends EventFiringWebDriver {
 	}
 
 	@After
-	public void close() {
+	public void close(ScenarioResult result) {
+		mkScreenshot(result);
 		super.close();
+	}
+
+	private void mkScreenshot(ScenarioResult result) {
+		try {
+			byte[] screenshot = this.getScreenshotAs(OutputType.BYTES);
+			result.embed(new ByteArrayInputStream(screenshot), "image/png");
+		} catch (WebDriverException somePlatformsDontSupportScreenshots) {
+			System.err
+					.println(somePlatformsDontSupportScreenshots.getMessage());
+		}
 	}
 }
